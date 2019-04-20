d9eb000f11fa54d7970730566673ef0d1b67d8fe
toberixng <tobyfatumo@gmail.com>
2019-04-20 16:06:27 +0100

    ripples first push with netlify

---
+++ ripple_beads/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import Slider from './components/Slider';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Slider />
+      </div>
+    );
+  }
+}
+
+export default App;
